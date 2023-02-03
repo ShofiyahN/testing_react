b8f67dfc24ac1ae4123c242e3e567f00d350c8bd
ShofiyahN <sofiyahnbs@gmail.com>
2023-02-03 16:40:30 +0800

    test2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import AppBarsLogin from './component/AppBars/AppBarsLogin';
 import { Route, Routes } from 'react-router-dom';
 import AppBarsRegis from './component/AppBars/AppBarsRegis';
 import AppBarsHome from './component/AppBars/AppBarsHome';
+import TestPage from './component/AppBars/TestPage';
 
 function App() {
   return (
     
       <Routes>
-        <Route path='/' element={<AppBarsRegis/>}/>
+        <Route path='/' element={<TestPage/>}/>
         <Route path='/login' element={<AppBarsLogin/>}/>
         <Route path='/home' element={<AppBarsHome/>}/>
       </Routes>
